53f5ccf62071f6fbea017ac3d758368bbe84d375
Waleed9211 <hafiz.waleed@nephrolytics.ai>
2025-03-04 03:20:03 +0500

    code updated to new repo

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import RecitationContainer from "./component/RecitationContainer";
+import RecitationContainer from "./component/RecitationContainer";
+import { RecitationProvider } from "./context/RecitationProvider";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import BecomeDev from './component/BecomeDev';
+import QuranSurahConverter from "./component/QuranSurahConverter";
+
+function App() {
+  return (
+    <RecitationProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RecitationContainer />} />
+        <Route path="/dev" element={<BecomeDev />} />
+        <Route path="/convert" element={<QuranSurahConverter />} />
+      </Routes>
+      </BrowserRouter>
+    </RecitationProvider>
+  );
+}
+
+export default App;
