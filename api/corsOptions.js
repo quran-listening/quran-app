c79002abdd7f0fcc3b20cde40a8b49e0b30fc712
Waleed9211 <hafiz.waleed@nephrolytics.ai>
2025-04-24 13:50:06 +0500

    cors options updated

--- api/corsOptions.js
+++ api/corsOptions.js
@@ -1,13 +1,9 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+
 
 export const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: (_origin, cb) => cb(null, true), 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  
   optionsSuccessStatus: 200,
   credentials: true, // Allow cookies to be sent
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
